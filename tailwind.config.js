74d966b4686cab90d6f9657aacffe52ee74adeb4
Eber Alejo <eber4202@hotmail.com>
2023-08-18 12:56:29 -0600

    FIX: mediaqueries

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {},
   darkMode: "class",
   plugins: [nextui()]
