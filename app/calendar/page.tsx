48e8579dea7b74eff9460584b9dce69cf3a8d96c
Eber Alejo <eber4202@hotmail.com>
2023-08-18 00:39:42 -0600

    REFACTOR: update calendar link

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -5,7 +5,7 @@ export default async function Calendar() {
         <h1 className="text-2xl font-bold mt-10">Calendar</h1>
         <p>Selecciona un horario disponible para tu cita y te enviaremos un correo confirmando tu cita al finalizar tu compra.</p>
         <div className="rounded-lg overflow-hidden h-80 m-10">
-          <iframe className='w-full h-full' src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FMexico_City&mode=WEEK&hl=es_419&showCalendars=0&showTabs=0&showDate=1&showTitle=0&src=ZmhvaDZmbDkzMzBydHJmNzBjdmFoYjQ4MG9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%2381a1c1"></iframe>
+          <iframe className='w-full h-full' src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FMexico_City&mode=WEEK&hl=es_419&showTabs=0&showCalendars=1&showDate=1&showNav=1&showTitle=0&src=YWY1ZmVjMTgyODBjNDQ2ZTNjOWIzNTlkMGQ4MDg5ZDZhMzUyZmI5N2YyYzIyOGU4OGRlMWNhYmE0OGNiZWNjY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23E67C73"></iframe>
         </div>
       </section>
     </>
