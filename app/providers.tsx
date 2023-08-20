f2e404ed2d7a7d2d882e139641fe5e5203c7c013
Eber Alejo <eber4202@hotmail.com>
2023-08-20 17:32:35 -0600

    STYLES: enable dark theme for MUI

--- app/providers.tsx
+++ app/providers.tsx
@@ -8,11 +8,25 @@ import { LocalizationProvider } from '@mui/x-date-pickers'
 // import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 export function Providers({ children }: { children: ReactNode }) {
+
+  // create theme for MUI
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
   return (
     <NextUIProvider>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        {children}
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
       </LocalizationProvider>
     </NextUIProvider>
   )
